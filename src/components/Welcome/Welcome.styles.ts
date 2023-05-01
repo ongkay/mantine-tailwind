6669cd46c46af252129be7ecb39e9ece30990464
ongkay <bisnismilyarandollar@gmail.com>
2023-05-01 09:14:39 +0700

    add mantineUI

---
+++ src/components/Welcome/Welcome.styles.ts
@@ -0,0 +1,14 @@
+import { createStyles } from '@mantine/core';
+
+export default createStyles((theme) => ({
+  title: {
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    fontSize: 80,
+    fontWeight: 700,
+    letterSpacing: -2,
+
+    [theme.fn.smallerThan('md')]: {
+      fontSize: 50,
+    },
+  },
+}));
